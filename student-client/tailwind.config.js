6be043ea9cf8ebcd81e30f7d7c80267b0d595427
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2022-09-22 01:48:29 -0500

    Add basic UI skeleton

--- student-client/tailwind.config.js
+++ student-client/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        userHighlight: '#1982FC'
+      }
+    },
   },
   plugins: [],
 }
