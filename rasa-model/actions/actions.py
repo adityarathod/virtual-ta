f9a05ea93f7e7830bd173fcae54ab70f759be187
bryanth24 <gbhou5@gmail.com>
2022-10-19 22:06:46 -0500

    added custom action for algorithm time complexity, updated readme and config for custom actions

--- rasa-model/actions/actions.py
+++ rasa-model/actions/actions.py
@@ -7,21 +7,36 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionAlgoTimeComplexity(Action):
+
+    def name(self) -> Text:
+        return "action_time_complexity"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        algo = tracker.get_slot('algorithm')
+
+        # to do: set up more robust data lookup (perhaps json)
+        algoToTime = {
+            "bfs": "O(V + E) where V == number of verticies and E == number of edges", 
+            "breadth first search": "O(V + E) where V == number of verticies and E == number of edges", 
+            "dfs": "O(V + E) where V == number of verticies and E == number of edges", 
+            "depth first search": "O(V + E) where V == number of verticies and E == number of edges", 
+            "binary search": "O(log(n))", 
+            "merge sort": "O(n * log(n))"
+            }
+
+        if algo in algoToTime: 
+            dispatcher.utter_message(text="The time complexity of " + algo + " is " + algoToTime[algo])
+        else: 
+            dispatcher.utter_message(text="I can't find that algorithm, please try a different spelling.")
+
+        return []
