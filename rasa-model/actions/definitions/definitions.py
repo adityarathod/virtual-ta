04a031c22cf002f57d98e834d4c30152d123b482
Trexrush <edmazuera@gmail.com>
2022-11-18 00:41:16 -0600

    added basic fuzzy search, optimized some training data for fuzzy

--- rasa-model/actions/definitions/definitions.py
+++ rasa-model/actions/definitions/definitions.py
@@ -5,9 +5,14 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import AllSlotsReset
 
+from thefuzz import process
+
 import json 
 #
 class ActionDefinitions(Action):
+
+    CONFIDENCE_THRESHOLD = 85
+    SIMILAR_THRESHOLD = 40
 
     def name(self) -> Text:
         return "action_definitions"
@@ -20,12 +25,20 @@ class ActionDefinitions(Action):
         print(topic)
         f = open('./definitions/data.json')
         topicToDefine = json.load(f)
-
         if topic in topicToDefine: 
             dispatcher.utter_message(text="The definition of " + topic + " is " + topicToDefine[topic])
         elif not topic: 
             dispatcher.utter_message(text="I don't understand, please try a different spelling or wording")
         else: 
-            dispatcher.utter_message(text="I can't find a definition for " + topic + ", please try a different spelling or wording.")
+            fuzzy_match = process.extract(topic, topicToDefine.keys(), limit=3)
+            print(fuzzy_match) # debug
+            if isinstance(fuzzy_match[0][1], int):
+                if fuzzy_match[0][1] > self.CONFIDENCE_THRESHOLD:
+                    dispatcher.utter_message(text="The definition of " + fuzzy_match[0][0] + " is " + topicToDefine[fuzzy_match[0][0]])
+                else:
+                    close_matches = [fuzzy_match[i][0] for i in range(len(fuzzy_match)) if fuzzy_match[i][1] > self.SIMILAR_THRESHOLD]
+                    dispatcher.utter_message(text="I believe " + topic + " may be misspelled, did you perhaps mean one of these? " + ', '.join(close_matches) + "?")
+            else: 
+                dispatcher.utter_message(text="I can't find a definition for " + topic + ", please try a different spelling or wording.")
         AllSlotsReset()
         return []
